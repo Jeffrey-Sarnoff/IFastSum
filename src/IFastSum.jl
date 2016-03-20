53dbe457b960c1d20127efbf02d83c3f430d479d
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 23:39:59 -0400

    Update IFastSum.jl

--- src/IFastSum.jl
+++ src/IFastSum.jl
@@ -85,7 +85,7 @@ function iFastSumAlgorithm{T<:Real}(x::Array{T,1},n::Int)
             w1, e1 = eftAdd(st, em)
             w2, e2 = eftAdd(st, -em)
             if ((w1+s != s) | (w2+s != s)) || (Round3(s,w1,e1) != s) || (Round3(s,w2,e2) != s)
-                re=1
+                re = 1
                 s1 = iFastSum(x,n) # first recursive call
                 s,s1 = eftAdd(s,s1)
                 s2 = iFastSum(x,n) # second recursive call
